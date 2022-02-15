4e07db76043f356f87ddae3b6f62b72ac70e26cd
Максим Пшибло <pshblo.max@gmail.com>
2022-02-15 23:50:58 +0300

    di with guice

--- src/main/java/ru/bstu/ai/guice/AiModule.java
+++ src/main/java/ru/bstu/ai/guice/AiModule.java
@@ -1,6 +1,11 @@
 package ru.bstu.ai.guice;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Scopes;
+import com.google.inject.name.Names;
+import ru.bstu.ai.core.service.Solution;
+import ru.bstu.ai.core.service.SolutionDepthImpl;
+import ru.bstu.ai.core.service.SolutionWideImpl;
 
 /**
  * инъекция всяких зависимостей (наших сервисов для ui).
@@ -9,5 +14,17 @@ public class AiModule extends AbstractModule {
     @Override
     protected void configure() {
 
+        bind(Solution.class)
+                .to(SolutionWideImpl.class);
+
+        bind(Solution.class)
+                .annotatedWith(Names.named("Depth"))
+                .to(SolutionDepthImpl.class)
+                .in(Scopes.SINGLETON);
+
+        bind(Solution.class)
+                .annotatedWith(Names.named("Wide"))
+                .to(SolutionWideImpl.class)
+                .in(Scopes.SINGLETON);
     }
 }
--- src/main/java/ru/bstu/ai/ui/AiApplication.kt
+++ src/main/java/ru/bstu/ai/ui/AiApplication.kt
@@ -1,7 +1,10 @@
 package ru.bstu.ai.ui
 
 import com.google.inject.Guice
+import com.google.inject.Key
+import com.google.inject.name.Names
 import javafx.stage.Stage
+import ru.bstu.ai.core.service.Solution
 import ru.bstu.ai.guice.AiModule
 import ru.bstu.ai.ui.views.MainView
 import tornadofx.App
@@ -16,6 +19,9 @@ class AiApplication: App(MainView::class) {
         FX.dicontainer = object : DIContainer {
             override fun <T : Any> getInstance(type: KClass<T>)
                     = guice.getInstance(type.java)
+
+            override fun <T : Any> getInstance(type: KClass<T>, name: String)
+                    = guice.getInstance(Key.get(type.java, Names.named(name)))
         }
     }
 
